49002928fad6a42a9ac52e299266df545c706fa5
Geetanjali Sahoo <Hemanta@Geetanjalis-Mac-mini.local>
2015-09-07 19:00:12 -0700

    sonar

---
+++ sonar/src/test/java/com/aexp/sonar/sonar/AppTest.java
@@ -0,0 +1,29 @@
+package com.aexp.sonar.sonar;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void test(){
+		
+		Assert.assertEquals("Test", "Test");
+	}
+	
+	@Test
+	public void test01(){
+		
+		Assert.assertEquals("Test", "Test");
+	}
+	
+	@Test
+	public void test02(){
+		
+		Assert.assertEquals("Test", "Test");
+	}
+  
+}
